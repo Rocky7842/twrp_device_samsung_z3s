dabbf62928763a9ced72e4fd22dc82e0eaa58242
Rocky7842 <49489159+Rocky7842@users.noreply.github.com>
2023-10-17 21:58:15 +0800

    z3s: Switch to TARGET_RECOVERY_DEVICE_DIRS

--- twrp_z3s.mk
+++ twrp_z3s.mk
@@ -7,12 +7,6 @@ $(call inherit-product, vendor/twrp/config/common.mk)
 # Inherit device configuration
 $(call inherit-product, device/samsung/z3s/device.mk)
 
-# Charger
-PRODUCT_PACKAGES += \
-    charger_res_images
-
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,device/samsung/z3s/recovery/root,recovery/root)
-
 # Device identifier
 PRODUCT_RELEASE_NAME := z3s
 PRODUCT_DEVICE := z3s
@@ -20,4 +14,3 @@ PRODUCT_NAME := twrp_z3s
 PRODUCT_BRAND := Samsung
 PRODUCT_MODEL := Galaxy S20 Ultra
 PRODUCT_MANUFACTURER := samsung
-
